c7ce014cae758ea48e526a6a8f8da7d595251ae5
joyh <whfprl10@gmail.com>
2021-05-19 16:40:50 +0900

    ADD modal ui
    모달을 추가하여 버튼 클릭시 켜고닫기가 가능하도록
    구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false); // modal을 켜고 닫는 스위치
   let posts = '강남 고기 맛집';
 
   function 제목바꾸기(){
@@ -37,11 +38,12 @@ function App() {
         <hr/>
       </div>
 
-      
-      
-      <Modal>
-        
-      </Modal>
+      <button onClick={ ()=>{modal변경(!modal)}}>버튼</button>
+      {
+        modal === true
+        ? <Modal />
+        : null
+      }
     </div>
   );
 }

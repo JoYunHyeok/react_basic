3a3a106023e43612d0ac32eab32ef1e70df3b552
joyh <whfprl10@gmail.com>
2021-05-25 03:45:02 +0900

    Update page for page
    프롭스기능을 배우고 적용해보았습니다.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false); // modal을 켜고 닫는 스위치
   let posts = '강남 고기 맛집';
+  let [누른제목, 누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+
 
   function 제목바꾸기(){
     var newArray = [...글제목];
@@ -20,38 +23,49 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ ()=>{따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[1] } </h3>
-        <p>2월 18일</p>
-        <hr/>
-        <p></p>
-      </div>
-      <div className="list">
-        <h3>{ 글제목[2] }</h3>
-        <p>2월 19일 발행</p>
-        <hr/>
-      </div>
 
-      <button onClick={ ()=>{modal변경(!modal)}}>버튼</button>
+
+      {
+        글제목.map(function(글, i){
+          return(
+            <div className="list" key={i}>
+              <h3 onClick={ ()=>{누른제목변경(i)}}> { 글 } <span onClick={ ()=>{따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
+              <p>2월 18일</p>
+              <hr/>
+              <p></p>
+            </div>
+          )
+        })
+      }
+      
+      <div className='publish'>
+        {입력값}
+        <input onChange={(e) => { 입력값변경(e.target.value)}}></input>
+        <button onClick={() => {
+          var newArray = [...글제목];
+          newArray.unshift(입력값)
+          글제목변경(newArray);
+         }}>버튼</button>
+      </div>
+      
+      {/* {입력값} */}
+      {/* <input onChange={ (e)=>{ 입력값변경(e.target.value) } }></input> */}
+
+  
+      <button onClick={ ()=>{modal변경(!modal)}}>열고닫기</button>
       {
         modal === true
-        ? <Modal />
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
       }
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className='modal'>
-      <h2>제목</h2>
+      <h2>{props.글제목[props.누른제목]}</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
